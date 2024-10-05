34b3a959ced3133b400562291bee7e21f36c55b0
Sebastian Soto <juansebastiansotomartin@gmail.com>
2024-10-04 21:57:22 -0300

    ..

--- aluraGeek.js
+++ aluraGeek.js
@@ -1,28 +1,63 @@
-document
-  .getElementById("productForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita que el formulario se envíe
+const productForm = document.getElementById("productForm");
+const productList = document.getElementById("product_list");
 
-    // Obtener valores de los inputs
-    const name = document.getElementById("name").value;
-    const price = document.getElementById("price").value;
-    const imageUrl = document.getElementById("imageUrl").value;
-
-    // Crear una nueva tarjeta de producto
+function renderProducts() {
+  productList.innerHTML = "";
+  const products = JSON.parse(localStorage.getItem("products")) || [];
+  products.map((product, index) => {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
-
-    // Insertar el contenido de la tarjeta
     productCard.innerHTML = `
-      <img src="${imageUrl}" alt="${name}" />
-      <h2>${name}</h2>
-      <p>Precio: $${price}</p>
-    `;
+            <img src="${product.imageUrl}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <div>
+              <p>Precio: $${product.price}.00</p>
+              <button class="delete-btn" data-index="${index}">
+                <img src="assets/basuraIcon.png" alt="Eliminar" >
+              </button>
+            </div>
+         
+        `;
 
-    // Añadir la tarjeta al contenedor de productos
-    const productsContainer = document.getElementById("products_container");
-    productsContainer.appendChild(productCard);
-
-    // Limpiar el formulario
-    document.getElementById("productForm").reset();
+    productList.appendChild(productCard);
   });
+}
+
+productForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const imageUrl = document.getElementById("imageUrl").value;
+
+  const product = { name, price, imageUrl };
+
+  const products = JSON.parse(localStorage.getItem("products")) || [];
+  products.push(product);
+
+  localStorage.setItem("products", JSON.stringify(products));
+
+  // Limpiar el formulario
+  productForm.reset();
+
+  // Volver a renderizar los productos
+  renderProducts();
+});
+
+productList.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("delete-btn") ||
+    event.target.closest(".delete-btn")
+  ) {
+    const index = event.target.getAttribute("data-index");
+
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+    products.splice(index, 1);
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    renderProducts();
+  }
+});
+
+window.addEventListener("DOMContentLoaded", renderProducts);
